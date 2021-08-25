90a144b430de306bc3c50f05398abd0e0dc2aad9
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2021-08-25 10:32:17 +0200

    API and map work; gotta work on styling

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,48 @@
 //DOM
-const address = document.querySelector('.info__text--address');
-const location = document.querySelector('.info__text--location');
-const timezone = document.querySelector('.info__text--timezone');
-const isp = document.querySelector('.info__text--isp');
+const formIP = document.querySelector('.header__form');
+const addressText = document.querySelector('.info__text--address');
+const locationText = document.querySelector('.info__text--location');
+const timezoneText = document.querySelector('.info__text--timezone');
+const ispText = document.querySelector('.info__text--isp');
 
 //API
-
 const apiKey = 'at_NR13vLwPZnckYQlgtecvHdVK3VMXi';
+formIP.addEventListener('submit', e=> {
+    e.preventDefault();
+    const ip= formIP.inputIP.value.trim();
 
-const getInfo = async () => {
-    const response = await fetch(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=8.8.8.8`);
+    
+const getInfo = async (ip) => {
+    const response = await fetch(`https://geo.ipify.org/api/v1?apiKey=${apiKey}&ipAddress=${ip}`);
+    if(response.status !== 200){
+        throw new Error('cannot fetch the data');
+    }
     const data = await response.json();
     return data;
 }
 
 
-getInfo().then(data => {
+getInfo(ip).then(data => {
     console.log(data, data.ip);
-    address.innerText = data.ip;
-    location.innerText = `${data.location.region}, ${data.location.city} ${data.location.postalCode}`
-    timezone.innerText = data.location.timezone;
-    isp.innerText = data.isp;
+    addressText.innerText = data.ip;
+    locationText.innerText = `${data.location.region}, ${data.location.city} ${data.location.postalCode}`
+    timezoneText.innerText = data.location.timezone;
+    ispText.innerText = data.isp;
+    map.setView([data.location.lat, data.location.lng], 13);
+
+    L.marker([data.location.lat, data.location.lng]).addTo(map)
+})
+    
     
 });
 
 
 
-
-
-
-// display map
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([34.04915, -118.09462], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([34.04915, -118.09462]).addTo(map)
     
