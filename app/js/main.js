c5589f511bcc729cf55d0492f3188fe5427a7001
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2021-08-25 18:28:48 +0200

    done

--- app/js/main.js
+++ app/js/main.js
@@ -44,5 +44,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([34.04915, -118.09462]).addTo(map)
+L.marker([34.04915, -118.09462],{riseOnHover:true}).addTo(map);
+
     
